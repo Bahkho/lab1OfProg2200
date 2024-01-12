85d69d9e18042d3fa2cb808728d23e7cbf0633ac
Abdul <akoredebakare4u@gmail.com>
2024-01-11 22:44:42 -0400

    Done with challenge 2

---
+++ Module1/challenge2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace challenge2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is the pass code?");
+        var code = Console.ReadLine();
+
+        if (code == "secret")
+        {
+            Console.WriteLine("Authenticated...");
+            Console.WriteLine("Chamge the pass code?");
+            var change = Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Not Authenticated...");
+        }
+    }
+}
---
+++ Module1/section6/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace section6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is your name?");
+        var name = Console.ReadLine();
+
+        Console.WriteLine("What is your age?");
+        var age = Console.ReadLine();
+
+        Console.WriteLine("What month were you born in?");
+        var month = Console.ReadLine();
+
+        Console.WriteLine("What's your favorite color?");
+        var color = Console.ReadLine();
+
+        Console.WriteLine("What's your favorite animal?");
+        var animal = Console.ReadLine();
+
+        Console.WriteLine("Your name is: {0}", name);
+        Console.WriteLine("Your age is: {0}", age);
+        Console.WriteLine("Your birth month is: {0}", month);
+        Console.WriteLine("Your favorite color is: {0}", color);
+        Console.WriteLine("Your favorite animal is: {0}", animal);
+
+        if (month == "march")
+        {
+            Console.WriteLine("You are an Aries.");
+        }
+        else if (month == "april")
+        {
+            Console.WriteLine("You are a Taurus.");
+        }
+        else if (month == "may")
+        {
+            Console.WriteLine("You are a Gemini.");
+        }
+        else
+        {
+            Console.WriteLine("You are a Cancer.");
+        }
+    }
+}
