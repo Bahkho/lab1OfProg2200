035f9a1ce636f4b903bb6fb1728ebae461636c9a
Abdul <akoredebakare4u@gmail.com>
2024-01-12 22:37:44 -0400

    done challenge 3

---
+++ Module1/challenge3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace challenge3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x <= 5; x++)
+            {
+                PrintNumbers(1, 10, true);
+                PrintNumbers(10, 1, false);
+            }
+        }
+
+        static void PrintNumbers(int start, int end, bool ascending)
+        {
+            int increment = ascending ? 1 : -1;
+
+            for (int i = start; ascending ? i <= end : i >= end; i += increment)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Module1/section9/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace section9;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // For loop
+        for (var i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        // While loop
+        var code = "";
+        while (code != "secret")
+        {
+            Console.WriteLine("What is the pass code?");
+            code = Console.ReadLine();
+
+            if (code != "secret")
+            {
+                Console.WriteLine("Not Authenticated...");
+
+            }
+        }
+        Console.WriteLine("Authenticated...");
+
+
+
+    }
+}
