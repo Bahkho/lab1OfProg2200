c0cca16fc317d7cc78be172c2f78f19d09b944ad
Abdul <akoredebakare4u@gmail.com>
2024-01-14 19:08:38 -0400

    update code

---
+++ Module1/section10/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace section10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var studentGrades = new int[10] { 80, 77, 45, 87, 53, 43, 76, 100, 99, 67 };
+
+        foreach (var studentGrade in studentGrades)
+        {
+            Console.WriteLine(studentGrade);
+        }
+    }
+}
---
+++ Module1/section11/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace section11;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter your name:");
+        var name = TryAgain();
+
+        Console.WriteLine("Please enter your age:");
+        var age = TryAgain();
+
+        Console.WriteLine("Please enter your month of birth:");
+        var month = TryAgain();
+
+        Console.WriteLine("Your name is {0}", name);
+        Console.WriteLine("Your age is {0}", age);
+        Console.WriteLine("Your month of birth is {0}", month);
+
+        if (month == "march")
+        {
+            Console.WriteLine("You are an Aries.");
+        }
+        else if (month == "april")
+        {
+            Console.WriteLine("You are a Taurus.");
+        }
+        else if (month == "may")
+        {
+            Console.WriteLine("You are a Gemini.");
+        }
+
+    }
+
+    static string TryAgain()
+    {
+        var answer = Console.ReadLine();
+        if (answer == "")
+        {
+            Console.WriteLine("You didn't type anything, please try again:");
+            return Console.ReadLine();
+        }
+        return answer;
+    }
+}
+
